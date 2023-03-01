ed6bc6f5351644241eda0bb36b4e07d7cc895dab
Oli-Goodall <oligoodall96@gmail.com>
2023-03-01 14:51:43 +0000

    Sorted CV Page content positioning

--- personal-site/src/components/cv-page/CVPage.tsx
+++ personal-site/src/components/cv-page/CVPage.tsx
@@ -5,6 +5,7 @@ export const CVPage: React.FunctionComponent = () => {
     return(
         <div className="cv-page">
             <h1 className="cv-page-heading">Curriculum Vitae</h1>
+            <h2 className="education">Education</h2>
         </div>
     )
 }
